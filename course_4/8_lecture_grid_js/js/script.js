16e802d457e175ba25778030b203e0e2ce41263e
Semen Soldatov <semsoldatow@yandex.ru>
2023-10-31 20:02:45 +0300

    add js lecture for 4 course

---
+++ course_4/8_lecture_grid_js/js/script.js
@@ -0,0 +1,26 @@
+'use strict'
+
+let widthContentOrHeight = '234'
+let isModalOpen
+
+// 1. camelСase
+// 2. snake_case
+// 3. kebab-case
+// 4. PascalCase
+// 5. UPPER_SNAKE_CASE
+
+const REST_API = ''
+const _apiKey = 'asdasdasdnasndka'
+
+let objStudent = {
+  age: 17,
+  name: 'Ivan',
+  mum_and_dad: 'Larisa and Ivan',
+}
+
+const isIvanHasPets = 'isHasDog'
+
+console.log(objStudent)
+//              0    1   3     4         5
+const mass = ['asd', 2, true, () => {}, {}]
+console.log(mass[1])
---
+++ course_4/8_lecture_grid_js/js/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+//  0    1   3     4         5
+const mass = ['asd', 2, true, () => {}, {}]
+console.log(mass[1])
